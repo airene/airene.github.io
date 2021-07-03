a9745c4f861bb57e011d4a8d46d1b3a8bd390b06
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-07-03 06:30:48 +0000

    Deploy airene/airene.github.io to github.com/airene/airene.github.io.git:gh-pages

--- assets/index.md.ce6f29e6.lean.js
+++
@@ -1,1 +0,0 @@
-import{r as e,o as t,c as a,a as r}from"./app.2f939950.js";const o='{"title":"Home","description":"","frontmatter":{"page":true,"home":true,"date":"2021-06-30","title":"Home","sidebar":false},"relativePath":"index.md","lastUpdated":1625202057485}',s={};s.render=function(o,s,d,i,n,l){const m=e("Home");return t(),a("div",null,[r(m)])};export default s;export{o as __pageData};
